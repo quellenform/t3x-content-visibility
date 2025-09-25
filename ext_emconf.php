0bf8c753b39eaab60df6a3d1a9d7ff5bf9216d28
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-09-25 17:46:02 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,21 +2,21 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Content Visibility',
-    'description' => '',
+    'description' => 'Control the visibility of content elements depending on the screen size.',
     'category' => 'fe',
-    'state' => 'alpha',
+    'state' => 'beta',
     'clearcacheonload' => true,
     'author' => 'Stephan Kellermayr',
     'author_email' => 'typo3@quellenform.at',
     'author_company' => 'Kellermayr KG',
-    'version' => '0.0.3',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.9.99'
+            'typo3' => '10.4.0-13.9.99'
         ],
         'conflicts' => [],
         'suggests' => [
-            'bootstrap_package' => '14.0.7-14.9.99'
+            'bootstrap_package' => '12.0.10-15.99'
         ]
     ]
 ];
